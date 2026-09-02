d307c3cba2dafedf221619060c011e64776afc16
Lucio.YipengLi <Lucio.YipengLi@outlook.com>
2026-09-02 21:42:35 +0800

    Fix: Add ignore __init__.py

---
+++ source/g1/g1/tasks/manager_based/g1/utils/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""This sub-module contains asset definitions that are specific to the environment."""
+
+from .assets import G1_29DOF_CFG
+
+__all__ = [
+    "G1_29DOF_CFG",
+]
